4191b3459f004955e39d839e58fced6820881445
JoseCarTorBel <josetorrobelda@gmail.com>
2021-01-09 09:58:16 +0100

    practica 04 terminada

--- PRACTICA_04_MEMORIA/cliente_mem.c
+++ PRACTICA_04_MEMORIA/cliente_mem.c
@@ -13,8 +13,15 @@ char *memoria;
 int TAMANYO_BLOQUE = 5000;
 
 
-unsigned long CR0, CR3, CR4, EFER, *direccion_inicio, direccion_inicio_bloque, ps;
+unsigned long CR0, *CR3, CR4, EFER, *direccion_inicio, direccion_inicio_bloque, ps;
 unsigned long PS, indexTP1, indexTP2, indexTP3, indexTP4, offset;
+
+/** 
+ * PDPTE = 1 Cuando es de 1 GB
+ * PDTE = 1 AND PDE = 1 Cuando es de 2 MB
+ * PDTE = 0 AND PDE = 0 Cuando es de 4 KB
+ */
+int PDPTE=0, PDE=0;
 
 unsigned long   flagTablaPaginas01, 
 	            flagTablaPaginas02, 
@@ -46,7 +53,6 @@ unsigned long   *direccionMarcoTP1Virtual,
 #define mascindexTP2 0x7fc0000000 /*Posiciones 38 - 30*/                       
 #define mascindexTP1 0xff8000000000 /* Posiciones 47 - 39 */
 
-#define mascaraPS 0x80
 
 module_param(numero_bloques, int, 0);
 
@@ -57,6 +63,7 @@ extern void libera_memoria(void);
 
 void muestra_info_bloques(unsigned long direccion_inicio_bloque);
 void imprime_TP();
+int checkPS(unsigned long flag);
 
 int init_module(void)
 {
@@ -67,6 +74,7 @@ int init_module(void)
     CR0 = read_cr0();
     CR3 = read_cr3();
     CR4 = read_cr4();
+    
     /**Se pone la máscara */
     EFER = native_read_msr(0xC0000080);
 
@@ -99,7 +107,7 @@ int init_module(void)
     for (i = 0; i < numero_bloques; i++)
     {	
 	direccion_inicio_bloque+=i*TAMANYO_BLOQUE;
-	printk("cliente_mem: Bloque %d ****************\n\n",i);
+	printk("\ncliente_mem: Bloque %d ****************\n\n",i);
 	printk("cliente_mem: Dirección física del bloque: 0x%016lX. Dirección virtual del bloque: 0x%016lX \n",
 		    (unsigned long)virt_to_phys((volatile void *)direccion_inicio_bloque),
 		    direccion_inicio_bloque);
@@ -130,78 +138,87 @@ void muestra_info_bloques(unsigned long direccion_inicio_bloque)
     */
     /*TABLA DE PÁGINA 1*/    
     flagTablaPaginas01 = (direccion_inicio[indexTP1]&mascaraFlags);
-    direccionMarcoTP1=(direccion_inicio[indexTP1]&mascaraMarco);
+    direccionMarcoTP1 = (direccion_inicio[indexTP1]&mascaraMarco);
     direccionMarcoTP1Virtual = (unsigned long *) phys_to_virt(direccionMarcoTP1);
+    printk("cliente_mem: Indice TP nivel 1: %lu(0x%3lX)\n",indexTP1,indexTP1);
+    printk("cliente_mem: Flags entrada TP nivel 1:0%lX\n",flagTablaPaginas01);
 
     /*Tabla de página 2*/
     flagTablaPaginas02 = (direccionMarcoTP1Virtual[indexTP2]&mascaraFlags);
     direccionMarcoTP2 = (direccionMarcoTP1Virtual[indexTP2]&mascaraMarco);
     direccionMarcoTP2Virtual = (unsigned long *) phys_to_virt(direccionMarcoTP2);
+    printk("cliente_mem: Indice TP nivel 2: %lu(0x%3lX)\n",indexTP2,indexTP2);
+    printk("cliente_mem: Flags entrada TP nivel 2:0%lX\n",flagTablaPaginas02);
 
-    /*Tabla de página 3*/
-    flagTablaPaginas03 = (direccionMarcoTP2Virtual[indexTP3]&mascaraFlags);
-    direccionMarcoTP3 = (direccionMarcoTP2Virtual[indexTP3]&mascaraMarco);
-    direccionMarcoTP3Virtual = (unsigned long *) phys_to_virt(direccionMarcoTP3);
-
-    ps = ((unsigned long)direccion_inicio_bloque&mascaraPS)>>7;
-
-    /* Tamaño de tabla de páginas*/
-    if(ps == 0) /* Página 4 KB*/
-    {   
-        indexTP4 = ((unsigned long) direccion_inicio_bloque&mascindexTP4) >>12; /*Desplazamos los 12 bits restantes, indice tabla de paginas 4*/
-        offset = ((unsigned long) direccion_inicio_bloque&mascaraDesp4K); /*Sacamos el offset*/
-        
-        /*Tabla de páginas 4*/
-        flagTablaPaginas04 = (direccionMarcoTP3Virtual[indexTP4]&mascaraFlags);
-        direccionMarcoTP4 = (direccionMarcoTP3Virtual[indexTP4]&mascaraMarco);
-        direccionMarcoTP4Virtual = phys_to_virt(direccionMarcoTP4);
-    }
-    else /* Página 2 MB*/
-    {
-        offset = ((unsigned long) direccion_inicio_bloque&mascaraDesp2M);        
-    }
     
-    imprime_TP();
+    PDPTE = checkPS(flagTablaPaginas02);
+    if(!PDPTE)/* Pagina de 2MB*/
+    {
+         /*Tabla de página 3*/
+        flagTablaPaginas03 = (direccionMarcoTP2Virtual[indexTP3]&mascaraFlags);
+        direccionMarcoTP3 = (direccionMarcoTP2Virtual[indexTP3]&mascaraMarco);
+        direccionMarcoTP3Virtual = (unsigned long *) phys_to_virt(direccionMarcoTP3);
+        printk("cliente_mem: Indice TP nivel 3: %lu(0x%3lX)\n",indexTP3,indexTP3);
+        printk("cliente_mem: Flags entrada TP nivel 3:0%lX\n",flagTablaPaginas03);
 
+        
+        PDE = checkPS(flagTablaPaginas03);
+        /* Tamaño de tabla de páginas*/
+        if(!PDE) /* Página 4 KB*/
+        {   
+            indexTP4 = ((unsigned long) direccion_inicio_bloque&mascindexTP4) >>12; /*Desplazamos los 12 bits restantes, indice tabla de paginas 4*/
+            
+            /*Tabla de páginas 4*/
+            flagTablaPaginas04       = (direccionMarcoTP3Virtual[indexTP4]&mascaraFlags);
+            direccionMarcoTP4        = (direccionMarcoTP3Virtual[indexTP4]&mascaraMarco);
+            direccionMarcoTP4Virtual = phys_to_virt(direccionMarcoTP4);
+            printk("cliente_mem: Indice TP nivel 4: %lu(0x%3lX)\n",indexTP4,indexTP4);
+            printk("cliente_mem: Flags entrada TP nivel 4:0%lX\n",flagTablaPaginas04);       
+            printk("cliente_mem: la pagina es de 4 KB\n");
+        }
+        else{
+            printk("cliente_mem: la pagina es de 2 MB\n");
+        }       
+    }else 
+    {
+        printk("clciente_mem: la página es de 1 GB\n");
+    }
+
+    imprime_TP();
 }
+
+/**
+ * Mira el PDE y PDTE según corresponda para saber si es de 4KG o 1 GB.
+ */
+int checkPS(unsigned long flag)
+{
+    int bit = flag>>7;
+    return bit&1;
+}
+
 
 void imprime_TP()
 {
-  
-    printk("cliente_mem: Indice TP nivel 1: %lu(0x%3lX)\n",indexTP1,indexTP1);
-    printk("cliente_mem: Flags entrada TP nivel 1:0%lX\n",flagTablaPaginas01);
-    
-    printk("cliente_mem: Indice TP nivel 2: %lu(0x%3lX)\n",indexTP2,indexTP2);
-    printk("cliente_mem: Flags entrada TP nivel 2:0%lX\n",flagTablaPaginas02);
-    
-    printk("cliente_mem: Indice TP nivel 3: %lu(0x%3lX)\n",indexTP3,indexTP3);
-    printk("cliente_mem: Flags entrada TP nivel 3:0%lX\n",flagTablaPaginas03);
-    
-    if(ps==0)
-    {
-        printk("cliente_mem: Indice TP nivel 4: %lu(0x%3lX)\n",indexTP4,indexTP4);
-        printk("cliente_mem: Flags entrada TP nivel 4:0%lX\n",flagTablaPaginas04);       
-        printk("cliente_mem: la pagina es de 4 KB\n");
-    } else {     
-      printk("cliente_mem: la pagina es de 2 MB\n");
-    }
-
     printk("cliente_mem: Dirección física del bloque: 0x%016lX. Dirección virtual del bloque: 0x%016lX \n",
             (unsigned long)virt_to_phys((volatile void *)direccion_inicio_bloque),
             direccion_inicio_bloque);
 
-    printk("cliente_mem: Marco TP nivel 1: Direccion fisica: 0x%016lX . Direccion virtual: 0x%016lX\n",direccionMarcoTP1,(unsigned long) direccionMarcoTP1Virtual);            
-    printk("cliente_mem: Marco TP nivel 2: Direccion fisica: 0x%016lX . Direccion virtual: 0x%016lX\n",direccionMarcoTP2,(unsigned long) direccionMarcoTP2Virtual);
-    printk("cliente_mem: Marco TP nivel 3: Direccion fisica: 0x%016lX . Direccion virtual: 0x%016lX\n",direccionMarcoTP3,(unsigned long) direccionMarcoTP3Virtual);
-    if(ps==0){
-        printk("cliente_mem: Marco TP nivel 4: Direccion fisica: 0x%016lX . Direccion virtual: 0x%016lX\n",direccionMarcoTP4,(unsigned long) direccionMarcoTP4Virtual);
-	printk("cliente_mem: Direccion del marco: 0x%016lX. Direccion de la página 0x%016lX\n", 
-	      (unsigned long)virt_to_phys((volatile void *)direccion_inicio_bloque)&mascaraMarco,
-	      direccion_inicio_bloque&mascaraDesp4K);   
-      
-    }else{    
-      printk("cliente_mem: Direccion del marco: 0x%016lX. Direccion de la página 0x%016lX\n", 
-	      (unsigned long)virt_to_phys((volatile void *)direccion_inicio_bloque)&mascaraMarco,
-	      direccion_inicio_bloque&mascaraDesp2M);    
+    printk("cliente_mem: Marco TP nivel 1: Direccion fisica: 0x%016lX. Direccion virtual: 0x%016lX\n",CR3,(unsigned long) direccion_inicio);            
+    printk("cliente_mem: Marco TP nivel 2: Direccion fisica: 0x%016lX. Direccion virtual: 0x%016lX\n",direccionMarcoTP1,(unsigned long) direccionMarcoTP1Virtual);
+    
+    if(!PDPTE)/* Pagina de 2MB*/
+    {
+        printk("cliente_mem: Marco TP nivel 3: Direccion fisica: 0x%016lX. Direccion virtual: 0x%016lX\n",direccionMarcoTP2,(unsigned long) direccionMarcoTP2Virtual);
+        if(!PDE){
+            printk("cliente_mem: Marco TP nivel 4: Direccion fisica: 0x%016lX . Direccion virtual: 0x%016lX\n",direccionMarcoTP3,(unsigned long) direccionMarcoTP3Virtual);
+            printk("cliente_mem: Direccion del marco: 0x%016lX. Direccion de la página 0x%016lX\n", 
+                    direccionMarcoTP4,
+                    (unsigned long) direccionMarcoTP4Virtual);   
+        
+        }else{    
+            printk("cliente_mem: Direccion del marco: 0x%016lX. Direccion de la página 0x%016lX\n", 
+                    direccionMarcoTP3,
+                    (unsigned long) direccionMarcoTP3Virtual);    
+        }
     }
 }
